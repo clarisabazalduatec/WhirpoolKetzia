01b5bae1542bcecba356f450ebaeb43ddf3fc67b
clarisabazaldua <118368190+clarisabazaldua@users.noreply.github.com>
2026-04-09 11:53:50 -0600

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -71,7 +71,7 @@ export default function LoginPage() {
   const handleGoogleLogin = async () => {
     setLoading(true);
     setError('');
-    await signIn('google', { callbackUrl: '/login' , prompt: 'select_account' }); // ELIMINAR PROMPT SI SE DESEA QUE GOOGLE RECUERDE LA SESSION Y ENTRE AUTOMATICAMENTE
+    await signIn('google', { callbackUrl: '/login', prompt: 'select_account', redirect: true }); // ELIMINAR PROMPT SI SE DESEA QUE GOOGLE RECUERDE LA SESSION Y ENTRE AUTOMATICAMENTE
   };
 
   return (
